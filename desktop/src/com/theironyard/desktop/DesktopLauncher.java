bf9a356cf72ffa2d71847cc7d2da1a9828271576
Erik <eschneider1128@gmail.com>
2016-06-02 12:12:36 -0400

    final with bug

--- desktop/src/com/theironyard/desktop/DesktopLauncher.java
+++ desktop/src/com/theironyard/desktop/DesktopLauncher.java
@@ -7,8 +7,6 @@ import com.theironyard.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 800;
-		config.height = 600;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
